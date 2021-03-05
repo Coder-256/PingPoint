5c68a0793c7f8ace3c6db4a0e26487e353c7c178
Jacob Greenfield <jacob@jacobgreenfield.me>
2021-03-05 11:28:23 -0500

    Initial Commit

---
+++ PingPoint/PingPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingPointApp.swift
+//  PingPoint
+//
+//  Created by Jacob Greenfield on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct PingPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
