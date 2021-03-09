083fb393b8a749c45bf2d998fcfe3dee84b72ff2
Jacob Greenfield <jacob@jacobgreenfield.me>
2021-03-09 10:07:54 -0500

    Switch to storyboard

---
+++ PingPoint/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PingPoint
+//
+//  Created by Jacob Greenfield on 3/9/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
